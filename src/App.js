0ab74f6ac112a5e1d4226ceec6695e70c249f918
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-01 20:59:12 -0500

    attemt at login for page threw errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
-import LogInScreen from './components/LogInScreen/LogInScreen';
+import LoginForm from './components/LoginForm/LoginForm';
 import Test from './components/Test';
 
 function App() {
+    const adminUser = {
+      email: 'admin@admin123',
+      password:'admin123'
+    }
+    
+    const [user, setUser] = useState({name:'', email:''});
+    const [error, setError] = useState('');
+
+    const Login = details =>{
+      console.log(details)
+    }
+    
+    const Logout = () =>{
+    console.log('logout');
+    }
   return (
     <div className="LogInWindow">
-      <LogInScreen/>
-      <Test/>
+      {(user.email != '')?(
+        <div className='welcome'>
+          <h2> Welcome, <span> {user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+      ):(
+        <LoginForm/>
+      )}
+      {/* <LogInScreen/>
+      <Test/> */}
     </div>
   );
 }
