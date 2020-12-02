8f8186e94d97938017068288fdc276fc3f1137a4
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-02 12:30:19 -0500

    Login test and Logout Test Working

--- src/App.js
+++ src/App.js
@@ -9,28 +9,32 @@ function App() {
       Password:'Test123'
     }
     
-    const [user, setUser] = useState({UserName:'', email:''});
+    const [user, setUser] = useState({UserName:'', Password:''});
     const [error, setError] = useState('');
 
     const Login = details =>{
       console.log(details);
 
       if (details.UserName == testUser.Username && details.Password == testUser.Password){
-    console.log('Logged In!')
-      }else{
+        console.log('Logged In!')
+        setUser({
+          Username: details.Username,
+          Password: details.Password
+        })
+        }else{
         console.log('Login Failuer')
       } 
     }
     
     const Logout = () =>{
-    console.log('logout');
+    setUser({ UserName:'',Password:''})
     }
   return (
     <div className="LogInWindow">
-      {(user.email != '')?(
+      {(user.Password != '')?(
         <div className='welcome'>
           <h2> Welcome, <span> {user.UserName}</span></h2>
-          <button>Logout</button>
+          <button onClick={Logout}>Logout</button>
         </div>
       ):(
         <LoginForm Login={Login} error={error}/>
